3016ad6e20fed810a22b5ab384bb833a8ad2f886
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2019-11-27 23:04:35 -0300

    Trabalho

---
+++ scripts/processElementData.js
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_Z_WPIRFVEeqTYpQOyEE6vwActivity"]="./../../Lean/capabilitypatterns/resources/LeanStartup_4920CF4C_4920cf4c_Activity.jpeg"
+contentPage.processPage.imageFiles["_Z_WPIRFVEeqTYpQOyEE6vwWPDependency"]="./../../Lean/capabilitypatterns/resources/LeanStartup_4920CF4C_4920cf4c_WPDependency.jpeg"
+contentPage.processPage.elementUrls["_eErs0BFVEeqTYpQOyEE6vw"] = ["Decisao de Pivotar","Lean/capabilitypatterns/decisao_de_pivotar_18D6E958.html"];
+contentPage.processPage.elementUrls["_e2Rh0BFVEeqTYpQOyEE6vw"] = ["Checar Metricas de Vaidade","Lean/capabilitypatterns/checar_metricas_de_vaidade_5EEFB19A.html"];
+contentPage.processPage.elementUrls["_gRlCMRFVEeqTYpQOyEE6vw"] = ["Construção do MVP","Lean/capabilitypatterns/construcao_do_mvp_1B0DF94C.html"];
+contentPage.processPage.elementUrls["_eErFwBFVEeqTYpQOyEE6vw"] = ["Equipe Startup","Lean/capabilitypatterns/equipe_startup_76597104.html"];
+contentPage.processPage.elementUrls["_eEoCcBFVEeqTYpQOyEE6vw"] = ["Analisar Progresso","Lean/capabilitypatterns/analisar_progresso_FB0F1E38.html"];
+contentPage.processPage.elementUrls["_e2Rh0xFVEeqTYpQOyEE6vw"] = ["Feedback dos Clientes","Lean/capabilitypatterns/feedback_dos_clientes_62C485D0.html"];
+contentPage.processPage.elementUrls["_eErFxBFVEeqTYpQOyEE6vw"] = ["Resultado da Checagem de Metricas","Lean/capabilitypatterns/resultado_da_checagem_de_metricas_3AEB5323.html"];
+contentPage.processPage.elementUrls["_gRlpQhFVEeqTYpQOyEE6vw"] = ["MVP","Lean/capabilitypatterns/mvp_42E1C4CB.html"];
+contentPage.processPage.elementUrls["_Z_WPIRFVEeqTYpQOyEE6vw"] = ["LeanStartup","Lean/capabilitypatterns/LeanStartup_4920CF4C.html"];
+contentPage.processPage.elementUrls["_gRlpQBFVEeqTYpQOyEE6vw"] = ["Equipe Startup","Lean/capabilitypatterns/equipe_startup_49AAD0A5.html"];
+contentPage.processPage.elementUrls["_e2Rh0RFVEeqTYpQOyEE6vw"] = ["Equipe Startup","Lean/capabilitypatterns/equipe_startup_698D91AA.html"];
+contentPage.processPage.elementUrls["_gRlCMhFVEeqTYpQOyEE6vw"] = ["Idealizaçao do MVP","Lean/capabilitypatterns/idealizacao_do_mvp_9A70D62.html"];
+contentPage.processPage.elementUrls["_gRlCMBFVEeqTYpQOyEE6vw"] = ["Apresentação aos Clientes","Lean/capabilitypatterns/apresentacao_aos_clientes_1070193C.html"];
+contentPage.processPage.elementUrls["_gRlpRBFVEeqTYpQOyEE6vw"] = ["discussao_MVP","Lean/capabilitypatterns/discussao_mvp_E3CB2C4.html"];
+contentPage.processPage.elementUrls["_eErFwhFVEeqTYpQOyEE6vw"] = ["Novas Métricas","Lean/capabilitypatterns/novas_metricas_6F90652A.html"];
